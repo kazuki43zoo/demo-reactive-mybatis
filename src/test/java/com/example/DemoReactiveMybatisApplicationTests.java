7d688dcf7eedccf6c3202cfe0c3c75d6f4044f2a
Kazuki Shimizu <kazuki43zoo@gmail.com>
2017-04-23 02:47:38 +0900

    Polishing testcase

--- src/test/java/com/example/DemoReactiveMybatisApplicationTests.java
+++ src/test/java/com/example/DemoReactiveMybatisApplicationTests.java
@@ -8,10 +8,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
+import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,28 +24,26 @@ public class DemoReactiveMybatisApplicationTests {
 	public void contextLoads() throws InterruptedException {
 		WebClient client = WebClient.create("http://localhost:" + port);
 
-		CountDownLatch countDownLatchForAssertion = new CountDownLatch(2);
-
-		final List<Todo> todos = new ArrayList<>();
-		client.get()
+		final List<Todo> todos = client.get()
 			.uri("/todos")
 			.exchange()
 			.flatMapMany(response -> response.bodyToFlux(Todo.class))
-			.subscribe(todo -> {
-				todos.add(todo);
-				countDownLatchForAssertion.countDown();
-			});
-
-		countDownLatchForAssertion.await(1, TimeUnit.SECONDS);
+			.collectList()
+			.block(Duration.ofSeconds(1));
 
 		assertThat(todos).hasSize(2);
-		assertThat(todos.get(0).getId()).isEqualTo(1);
-		assertThat(todos.get(0).getTitle()).isEqualTo("飲み会");
-		assertThat(todos.get(0).getDetails()).isEqualTo("4/22 19:00 銀座");
-		assertThat(todos.get(1).getId()).isEqualTo(2);
-		assertThat(todos.get(1).getTitle()).isEqualTo("お茶会");
-		assertThat(todos.get(1).getDetails()).isEqualTo("4/24 20:00 銀座");
-
+		{
+			Todo todo = todos.get(0);
+			assertThat(todo.getId()).isEqualTo(1);
+			assertThat(todo.getTitle()).isEqualTo("飲み会");
+			assertThat(todo.getDetails()).isEqualTo("4/22 19:00 銀座");
+		}
+		{
+			Todo todo = todos.get(1);
+			assertThat(todo.getId()).isEqualTo(2);
+			assertThat(todo.getTitle()).isEqualTo("お茶会");
+			assertThat(todo.getDetails()).isEqualTo("4/24 20:00 銀座");
+		}
 	}
 
 }
